e5652d1fc845f3d9b2566dd71a4effa482b2996d
Kota Subrahmanya Kashyap <64368285+kotakash@users.noreply.github.com>
2023-06-21 12:07:59 +0530

    Updated main.py

--- src/main.py
+++ src/main.py
@@ -78,4 +78,4 @@ def validate_user():
 	
 		
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=config.PORT, debug=config.DEBUG_MODE)
+    app.run(host="0.0.0.0", port=config.port, debug=config.debug_mode)
