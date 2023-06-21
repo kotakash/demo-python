e01c633f0836e9c993ee088f144fe379f1184bb5
EC2 Default User <ec2-user@ip-172-31-80-175.ec2.internal>
2023-06-21 06:28:29 +0000

    first commit

---
+++ src/config.py
@@ -0,0 +1,9 @@
+from os import environ as env
+
+port = 8080
+debug_mode = int(env.get("DEBUG_MODE", 1))
+mysql_host = "34.30.206.155"
+mysql_port = 3306
+mysql_user = "root"
+mysql_pass = "mule1995"
+mysql_db = "UID"
---
+++ src/main.py
@@ -0,0 +1,81 @@
+import mysql.connector
+from flask import jsonify
+from flask import Flask, request
+from flask_cors import CORS
+
+import config
+
+app = Flask(__name__)
+CORS(app)
+cors = CORS(app,resources={
+		r"/*": {
+			"origins": "*"
+		}
+})
+
+@app.route("/Hello")
+def hello():
+	print("Hi")
+	return "Hello World!"		
+
+		
+@app.route('/users',methods=['GET'])
+def users():
+	
+	db_connection = mysql.connector.connect(
+	host=config.mysql_host,
+	user=config.mysql_user,
+	passwd=config.mysql_pass,
+	database=config.mysql_db
+	)
+	my_database = db_connection.cursor()
+	sql_statement = "SELECT * FROM UID"
+	my_database.execute(sql_statement)
+	output = my_database.fetchall()
+	resp = jsonify(output)
+	return resp
+		
+@app.route('/users', methods=['POST'])
+def add_user():
+	_json = request.json
+	_firstname = _json['FirstName']
+	_lastname = _json['LastName']
+	_uid = _json['UID']
+	sql = "INSERT INTO UID(UID, FirstName, LastName) VALUES(%s, %s, %s)"
+	data = (_uid, _firstname, _lastname)
+	db_connection = mysql.connector.connect(
+	host=config.mysql_host,
+	user=config.mysql_user,
+	passwd=config.mysql_pass,
+	database=config.mysql_db
+	)
+	conn = db_connection.cursor()
+	conn.execute(sql, data)
+	db_connection.commit()
+	return "Record Inserted"
+	
+@app.route('/validate', methods=['POST'])
+def validate_user():
+	_json = request.json
+	_firstname = _json['FirstName']
+	_lastname = _json['LastName']
+	_uid = _json['UID']
+	db_connection = mysql.connector.connect(
+	host=config.mysql_host,
+	user=config.mysql_user,
+	passwd=config.mysql_pass,
+	database=config.mysql_db
+	)
+	conn = db_connection.cursor()
+	sql = "SELECT * FROM UID WHERE UID=%s And FirstName=%s And LastName=%s"
+	data = (_uid,_firstname,_lastname)
+	conn.execute(sql,data)
+	row = conn.fetchone()
+	if row is None:
+		return jsonify("UID not found. Please try again !")
+	else: 
+		return jsonify("Validation Successful")
+	
+		
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=config.PORT, debug=config.DEBUG_MODE)
